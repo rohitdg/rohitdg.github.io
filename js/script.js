c054d2f12d3446dd5224467bca79810907bb17af
Rohit Reddy D <rohit.dega@gmail.com>
2015-01-11 19:55:53 +0530

    Portfolio site in Jekyll

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  // $(window).scroll(function() {
+  // if ($(document).scrollTop() > 0) {
+  //   alert('hello');
+  //   });
+  
+  $(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $(".top").addClass('scroll_up');
+    $(".middle").addClass('scroll_up_middle')
+  } else {
+    $(".top").removeClass('scroll_up');
+    $(".middle").removeClass('scroll_up_middle');
+  }
+
+  });
+
+
+  $(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  });
+
+});
